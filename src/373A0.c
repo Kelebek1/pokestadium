8e627f9e68396906e750a43afa40dd610263b4e1
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-10-26 11:20:23 +0100

    aa

---
+++ src/373A0.c
@@ -0,0 +1,102 @@
+#include "common.h"
+/*
+? func_80038B68(s32*);
+s32 func_80039024(void*, void*, s32, s32, u32, s32);
+? func_800392A8(s32, s32);
+s32 func_80039354(s32);
+? func_8003979C(void*, s32);
+? func_800397BC(void*);
+? func_8003CADC();
+s32 func_800479C0(?, ?, ?, s32);
+? func_8004AD40(?);
+? func_8004ADB0(s32, ALSeqFile*, s32, s32);
+? func_8004AE90(?, ?);
+? func_80050B40(ALSeqFile*, void*, ?);
+void func_80037360(ALSeqFile* arg1, s32 arg2, s32 arg2);
+extern ? D_15C0000;
+extern ? D_80077970;
+extern s32 D_80077C64;
+extern s32 D_80077C68;
+extern s32 D_80077C6C;
+extern s32 D_80077C70;
+extern s32 D_80077C74;
+extern s32 D_80077C78;
+extern s32 D_80077C7C;
+extern s32 D_80079014;
+extern s8 D_80079364;
+extern ? D_800B32A0;
+extern void* D_800FC680;
+extern void* D_800FC684;
+extern void* D_800FC688;
+extern void* D_800FC68C;
+extern void* D_800FC690;
+extern ? D_800FC698;
+extern void* D_800FC6A4;
+extern void* D_800FC6A8;
+extern void* D_800FC6AC;
+extern ? D_800FC6B0;
+extern ? D_800FC6C0;
+extern ALSeqFile* D_800FC6CC;
+extern void* D_800FC6D0;
+extern ALSeqFile* D_800FC6D4;
+extern void* D_800FC6D8;
+extern ALSeqFile* D_800FC6DC;
+extern ALSeqFile* D_800FC6E0;
+extern ALSeqFile* D_800FC6E4;
+extern ALSeqFile* D_800FC6E8;
+extern ALSeqFile* D_800FC6EC;
+extern ALSeqFile* D_800FC6F0;
+extern ALSeqFile* D_800FC6F4;
+extern ALSeqFile* D_800FC6F8;
+extern ALSeqFile* D_800FC6FC;
+extern ALSeqFile* D_800FC700;
+extern ALSeqFile* D_800FC704;
+extern ALSeqFile* D_800FC708;
+extern ALSeqFile* D_800FC714;
+extern ALSeqFile* D_800FC798;
+extern ALHeap* D_800FC810;
+extern s32 D_800FC820;
+
+s32 func_80037234(s32 arg0, s32 arg1) {
+    switch (arg0) {
+        case 0x0:
+            if (arg1 == 0) {
+                arg1 = 1;
+            }
+            func_800392A8(D_80077C64, arg1);
+            D_80077C64 = 0;
+            return 1;
+        case 0x4:
+            if (arg1 == 0) {
+                arg1 = 1;
+            }
+            func_800392A8(D_80077C68, arg1);
+            return 1;
+        case 0x3:
+            func_800392A8(D_80077C6C, 0x3C);
+            D_80077C6C = 0;
+            return 1;
+        case 0xA1:
+            func_800392A8(D_80077C70, 0x78);
+            D_80077C70 = 0;
+            return 1;
+        case 0x5A:
+            func_800392A8(D_80077C74, 0x5A);
+            D_80077C74 = 0;
+            return 1;
+        case 0x5B:
+            func_800392A8(D_80077C78, 0x5A);
+            D_80077C78 = 0;
+            return 1;
+    }
+    return 0;
+}
+
+void func_80037338(void) {
+
+}
+
+void func_80037340(void) {
+    func_8003CADC();
+}
+*/
